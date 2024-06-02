e188b60738406bb8a688e099d9fd9cbe52b4f245
devskibb <devskibb@gmail.com>
2024-06-02 18:26:21 +0100

    Update portfolio-value.js

--- api/portfolio-value.js
+++ api/portfolio-value.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 export default async (req, res) => {
-  const covalentApiKey = process.env.COVALENT_API_KEY;
-  const coingeckoApiUrl = process.env.COINGECKO_API_URL;
-
-  console.log('Covalent API Key:', covalentApiKey);
-  console.log('Coingecko API URL:', coingeckoApiUrl);
-
-  if (!covalentApiKey || !coingeckoApiUrl) {
-    console.error('Environment variables are not set correctly.');
-    res.status(500).json({ error: 'Environment variables are not set correctly.' });
-    return;
-  }
-
   try {
+    const covalentApiKey = process.env.COVALENT_API_KEY;
+    const coingeckoApiUrl = process.env.COINGECKO_API_URL;
+
+    console.log('Covalent API Key:', covalentApiKey);
+    console.log('Coingecko API URL:', coingeckoApiUrl);
+
+    if (!covalentApiKey || !coingeckoApiUrl) {
+      console.error('Environment variables are not set correctly.');
+      res.status(500).json({ error: 'Environment variables are not set correctly.' });
+      return;
+    }
+
     console.log('Attempting to fetch data from CoinGecko...');
     const response = await axios.get(`${coingeckoApiUrl}/simple/price`, {
       params: { ids: 'bitcoin', vs_currencies: 'usd' }
@@ -21,7 +21,7 @@ export default async (req, res) => {
     console.log('Data fetched successfully from CoinGecko:', response.data);
     res.status(200).json({ price: response.data });
   } catch (error) {
-    console.error('Failed to fetch data from CoinGecko:', error);
+    console.error('Failed to fetch data from CoinGecko:', error.message);
     res.status(500).json({ error: 'Failed to fetch data from CoinGecko.' });
   }
 };
