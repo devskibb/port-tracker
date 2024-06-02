7837232427a7c7d765817a9868e844eb4cfe1624
devskibb <devskibb@gmail.com>
2024-06-02 18:16:11 +0100

    Update portfolio-value.js

--- api/portfolio-value.js
+++ api/portfolio-value.js
@@ -1,3 +1,11 @@
 export default (req, res) => {
-  res.status(200).json({ message: 'Server is running' });
+  const covalentApiKey = process.env.COVALENT_API_KEY;
+  const coingeckoApiUrl = process.env.COINGECKO_API_URL;
+
+  if (!covalentApiKey || !coingeckoApiUrl) {
+    res.status(500).json({ error: 'Environment variables are not set correctly.' });
+    return;
+  }
+
+  res.status(200).json({ message: 'Environment variables are set correctly.' });
 };
